3547f02e861072df781baf9e8299f12983f6523b
Blockman_ <65733896+Blockman02135@users.noreply.github.com>
2021-01-03 10:26:03 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Client } = require('discord.js');
+const client = new Client();
+const fetch = require('node-fetch');
+const keys = require('./keys.json');
+const Bot = require('./bot.js');
+
+var database = {};
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  var cnl = client.channels.cache.get(keys["dbFilesChannel"]);
+  cnl.messages.fetch(cnl.lastMessageID).then(m=>m.attachments.size&&(fetch(m.attachments.first().attachment).then(_=>_.text()).then(db=>{
+    database = db;
+    Bot(database,keys["token"],cnl);
+    console.log(database);
+  })));
+});
+
+client.login(keys["token"]);
