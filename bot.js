d86c3e36d707b4ed7ab71a56352744d40c04bf75
Blockman_ <65733896+Blockman02135@users.noreply.github.com>
2021-01-03 10:43:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
-const {Client, MessageAttachment} = require('discord.js');
-const client = new Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const MessageAttachment = Discord.MessageAttachment;
 const keys = require('./keys.json');
 const fs = require('fs');
 const commands = require('./commands.js')
